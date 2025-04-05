8ae3e234e9b99401ee7c6a931b2980b8ed79cf4f
Ping Yuan <pingyuanong05@gmail.com>
2025-04-06 01:37:03 +0800

    main update

--- assets/js/loadsection.js
+++ assets/js/loadsection.js
@@ -1,20 +1,19 @@
 // loadSections.js
 function loadSection(url, targetId) {
-    fetch(url)
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById(targetId).innerHTML = html;
-      })
-      .catch(error => {
-        console.error(`Error loading ${url}:`, error);
-      });
-  }
-  
-  document.addEventListener('DOMContentLoaded', () => {
-    loadSection('../greenhouse.html', 'greenhousesection');
-    loadSection('../impact.html', 'impactsection');
-    loadSection('../airquality.html', 'airqualitysection');
-    loadSection('../urbanization.html', 'urbanizationsection');
-    loadSection('../tempUhi.html', 'tempUHIsection');
-  });
-  
+  fetch(url)
+    .then(response => response.text())
+    .then(html => {
+      document.getElementById(targetId).innerHTML = html;
+    })
+    .catch(error => {
+      console.error(`Error loading ${url}:`, error);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  loadSection('../greenhouse.html', 'greenhousesection');
+  loadSection('../impact.html', 'impactsection');
+  loadSection('../airquality.html', 'airqualitysection');
+  loadSection('../urbanization.html', 'urbanizationsection');
+  loadSection('../tempUhi.html', 'tempUHIsection');
+});
