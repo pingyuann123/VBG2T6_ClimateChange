4e0b0ddacca6d1d57a35a3fc5e34450186b35bc8
Ping Yuan <pingyuanong05@gmail.com>
2025-04-05 10:48:04 +0800

    updated

---
+++ assets/js/loadsection.js
@@ -0,0 +1,16 @@
+// loadSections.js
+function loadSection(url, targetId) {
+    fetch(url)
+      .then(response => response.text())
+      .then(html => {
+        document.getElementById(targetId).innerHTML = html;
+      })
+      .catch(error => {
+        console.error(`Error loading ${url}:`, error);
+      });
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    loadSection('./urbanization.html', 'urbanizationsection');
+  });
+  
